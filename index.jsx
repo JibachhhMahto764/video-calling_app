46076381ef901651cd039302dc0c6b32a59d2137
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-04-10 09:39:53 +0545

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+    const [roomCode,setRoomCode] = useState("");
+    const navigate = useNavigate();
+    const handleFormSubmit = (ev) => {
+        ev.preventDefault();
+        navigate('/room/${roomCode}')
+
+    }
+    return (
+        <div className="home-page">
+            <form onSubmit={handleFormSubmit} className="form">
+             <div>
+                <label> Enter Room Code</label>
+                <input  value={roomCode} onChange = {e => setRoomCode(e.target.value)}type="text" required placeholder="Enter Room code" />
+
+             </div>
+             <button type="submit">Enter Room</button>
+            </form>
+        </div>
+    );
+};
+export default HomePage;
