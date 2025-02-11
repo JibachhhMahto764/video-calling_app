d0dff9ac9922abd4650d0adce81cbee09cf3a2c1
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2025-02-11 09:28:51 +0545

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -13,8 +13,19 @@ const HomePage = () => {
         <div className="home-page">
             <form onSubmit={handleFormSubmit} className="form">
              <div>
+                 
                 <label> Enter Room Code</label>
                 <input  value={roomCode} onChange = {e => setRoomCode(e.target.value)}type="text" required placeholder="Enter Room code" />
+
+             </div>
+                <div>
+                <label> Enter Name</label>
+                <input  value={roomName} onChange = {e => setRoomName(e.target.value)}type="text" required placeholder="Enter Your Name" />
+
+             </div>
+                <div>
+                <label> Enter Password</label>
+                <input  value={roomPassword} onChange = {e => setRoomPassword(e.target.value)}type="text" required placeholder="Enter Password" />
 
              </div>
              <button type="submit">Enter Room</button>
